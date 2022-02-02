e3f1fdf0128586bcd785ad54baf6fab9ca07b068
hannah <hannahtran481@gmail.com>
2022-02-02 10:35:58 -0800

    test

--- src/RunMe.java
+++ src/RunMe.java
@@ -4,7 +4,7 @@ public class RunMe {
     public static void main(String[] args) {
         // --== Load an image to filter ==--
         DisplayWindow.showFor("images/6.jpg", 800, 600);
-
+        //Hannah!
         // --== Determine your input interactively with menus ==--
         //DisplayWindow.getInputInteractively(800,600);
     }
